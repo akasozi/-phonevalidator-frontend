9f1a1774a0c0b317f358ddec01b6e21537ce61c0
Abu Kasozi <abukasozi@gmail.com>
2022-02-22 01:51:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './table.css';
 
 function App() {
-  return (
+  const [data, setData] = useState([]);
+  const [keyword, setKeyword] = useState('');
+  const [status, setStatus] = useState("⬇️ Select Status");
+
+  const fetchData = () => {
+    // const url =`https://api.github.com/search/repositories?q=${keyword}`;
+    const url = `http://localhost:8081/api/v1/phone-numbers?pageNo=0&pageSize=50&sortBy=id&country=${keyword}&state=${status}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      setData(response);
+    });
+  }
+
+  const statuses = [
+    { label: "VALID", value: "VALID" },
+    { label: "NOT VALID", value: "NOT_VALID" }
+  ]
+  
+
+  const handleFruitChange = (e) => {
+    setStatus(e.target.value)
+  }
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  }
+
+  const tableRows = data.map((item, index) =>
+  <tr key={index}>
+         <td>{item.phoneNumber}</td>
+         <td>{item.status}</td>
+  </tr>);
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <input type="text" onChange={handleChange} />
+        <button onClick={fetchData} value={keyword}>fetch</button>
+        
+        <br />
+        
+        <select onChange={handleFruitChange}> 
+            {/* Creating the default / starting option for our 
+              dropdown.
+            */}
+          <option value="⬇️ Select a fruit ⬇️"> -- Select a Status -- </option>
+          {statuses.map((status) => <option value={status.value}>{status.label}</option>)}
+        </select>
+      
+
+        <h3>List of Phone Numbers</h3>
+        <table id='phonenumbers'>
+              <tr>
+                <th>Phone Number</th>
+                <th>Status</th>
+              </tr>
+        <tbody>{tableRows}</tbody></table>
+      </div>
+    </div> 
+);
+
 }
 
 export default App;
